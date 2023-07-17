75448bfdd3aae6b7802354d1fdce211e15584408
FelipeArredond <pipechiqui77@gmail.com>
2023-07-17 18:26:15 -0500

    Class 4 - Spirng Data Course

---
+++ src/main/java/com/platzi/pizza/persistence/entities/PizzaEntity.java
@@ -0,0 +1,27 @@
+package com.platzi.pizza.persistence.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "pizza")
+@Getter
+@Setter
+@NoArgsConstructor
+public class PizzaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_pizza", nullable = false)
+    private Long idPizza;
+    @Column(nullable = false, length = 30, unique = true)
+    private String name;
+    @Column(nullable = false, length = 150)
+    private String description;
+    @Column(nullable = false, columnDefinition = "Decimal(5,2)")
+    private Double price;
+    private Boolean vegetarian;
+    @Column(nullable = false)
+    private Boolean available;
+}
