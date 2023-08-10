1153706a494ec778e342ad115177fe42f0336c73
FelipeArredond <pipechiqui77@gmail.com>
2023-08-09 23:27:31 -0500

    Se creo sistema de roles directamente en la base de datos

--- src/main/java/com/platzi/pizza/application/UserSecurityService.java
+++ src/main/java/com/platzi/pizza/application/UserSecurityService.java
@@ -1,6 +1,7 @@
 package com.platzi.pizza.application;
 
 import com.platzi.pizza.persistence.entities.UserEntity;
+import com.platzi.pizza.persistence.entities.UserRoleEntity;
 import com.platzi.pizza.persistence.repositories.UserRepository;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -21,11 +22,12 @@ public class UserSecurityService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         UserEntity userEntity = this.userRepository.findById(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User" + username + " not found"));
+        String[] roles = userEntity.getRoles().stream().map(UserRoleEntity::getRole).toArray(String[]::new);
         return User
                 .builder()
                 .username(userEntity.getUsername())
                 .password(userEntity.getPassword())
-                .roles("ADMIN")
+                .roles(roles)
                 .accountLocked(userEntity.getLocked())
                 .disabled(userEntity.getDisabled())
                 .build();
